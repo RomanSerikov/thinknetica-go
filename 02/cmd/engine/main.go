74954a9e776c190cf26ce39134d6320c6c87b746
Roman Serikov <roman.serikov@yandex.ru>
2020-10-18 04:29:28 +0300

    Added: simple search engine

---
+++ 02/cmd/engine/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/romanserikov/thinknetica-go/02/pkg/spider"
+)
+
+const exit = "exit"
+
+func main() {
+	sites := []string{
+		"https://habr.com",
+	}
+
+	store := make(map[string]string)
+	for _, site := range sites {
+		fmt.Printf("Scanning %s\n", site)
+
+		data, err := spider.Scan(site, 2)
+		if err != nil {
+			log.Fatalf("error during scanning %s: %v\n", site, err)
+		}
+
+		for url, title := range data {
+			store[url] = title
+		}
+	}
+
+	var input string
+
+	for {
+		fmt.Printf("Please, enter search request. Type %s to stop.\n", exit)
+		fmt.Scanln(&input)
+
+		if input == exit {
+			fmt.Println("See you next time.")
+			return
+		}
+
+		request := strings.ToLower(input)
+		response := make(map[string]string)
+
+		for url, title := range store {
+			if strings.Contains(url, request) || strings.Contains(strings.ToLower(title), request) {
+				response[url] = title
+			}
+		}
+
+		if len(response) == 0 {
+			fmt.Println("Sorry, nothing found.")
+			continue
+		}
+
+		fmt.Println("Results:")
+		for url, title := range response {
+			fmt.Printf("  * %s - %s\n", url, title)
+		}
+	}
+}
---
+++ 02/go.mod
@@ -0,0 +1,5 @@
+module github.com/romanserikov/thinknetica-go/02
+
+go 1.13
+
+require golang.org/x/net v0.0.0-20201016165138-7b1cca2348c0
